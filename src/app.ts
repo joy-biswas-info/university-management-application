699183cba2ff92ebdc87219c5a5a4494b81e8086
Joy Biswas <contact.joybiswas@gmail.com>
2023-06-08 21:30:22 +0600

    [DONE] Express, Mongoos, cors

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors'
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+
+
+export default app;
