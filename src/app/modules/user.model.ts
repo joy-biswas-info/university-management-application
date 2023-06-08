614c0eded8aa77241f1fc40ee5559032b9ff16e3
Joy Biswas <contact.joybiswas@gmail.com>
2023-06-08 23:12:21 +0600

    [done] eslint, husky, prettier,lint staged

---
+++ src/app/modules/user.model.ts
@@ -0,0 +1,15 @@
+import { Schema } from 'mongoose'
+import { IUser } from './users.interface'
+
+export const userSchema = new Schema<IUser>({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+})
